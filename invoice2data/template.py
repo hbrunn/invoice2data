3ebb595993072a6b550c7782e75ee7a2a7416424
Holger Brunn <hbrunn@therp.nl>
2017-08-31 14:42:31 +0200

    [ADD] protect against None values

--- invoice2data/template.py
+++ invoice2data/template.py
@@ -214,7 +214,7 @@ class InvoiceTemplate(OrderedDict):
                     if current_row:
                         lines.append(current_row)
                     current_row = {
-                        field: value.strip()
+                        field: (value or '').strip()
                         for field, value in match.groupdict().iteritems()
                     }
                     continue
